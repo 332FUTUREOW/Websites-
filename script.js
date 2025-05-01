cacdd2b359a3a12209c0ff7b1be8bcf9afed6fe8
332FUTUREOW <pp1444453@gmail.com>
2025-05-01 21:28:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 document.querySelector("button").addEventListener("click", function() {
   alert("Button clicked!");
 });
+const greeting = document.createElement('h2');
+const hour = new Date().getHours();
+
+if (hour < 12) {
+    greeting.textContent = "Good Morning!";
+} else if (hour < 18) {
+    greeting.textContent = "Good Afternoon!";
+} else {
+    greeting.textContent = "Good Evening!";
+}
+
+document.body.prepend(greeting);
